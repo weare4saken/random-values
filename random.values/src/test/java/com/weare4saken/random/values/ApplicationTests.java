2333b8f8d1fa8a4f6220a7e368c919eb2abce861
Ilya Kondratyuk <ikondratyuk@systematica-consulting.ru>
2024-01-15 16:48:57 +0700

    Initial commit

---
+++ random.values/src/test/java/com/weare4saken/random/values/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weare4saken.random.values;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
